fe5269baa99fcba71d015d62a72b40cc7a220bbc
Annemijn Dijkhuis <annemijndijkhuis@vpn-byods-145-109-54-97.vpn.uva.nl>
2020-06-16 17:19:47 +0200

    toch met 0 1 2

--- spatial/spatial_CA.py
+++ spatial/spatial_CA.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 SIZE = 100
+SURVIVAL = {0:0.8, 1:0.8}
+EMPTY_CELLS = 0.2
 
 
 def initialise():
@@ -12,8 +14,17 @@ def initialise():
     grid = np.random.randint(low=0,high=2,size=(size,size))
 
     # create allele grids
-    grid_a = np.random.randint(low=0, high=2,size=(size,size))
-    grid_b = np.random.randint(low=0, high=2,size=(size,size))
+    grid_a = np.random.randint(low=1, high=3,size=(size,size))
+    grid_b = np.random.randint(low=1, high=3,size=(size,size))
+
+    # oeps toch een for loop
+    for i in range(len(grid_a)):
+        for j in range(len(grid_a[0])):
+            r = np.random.uniform(0, 1)
+            if r > 0.8:
+                grid_a[i][j] = 0
+                grid_b[i][j] = 0
+
 
     return grid, grid_a, grid_b
 
@@ -31,5 +42,6 @@ def dispersal(grids):
 
 
 grid, grid_a, grid_b = initialise()
-
+print(grid_a[0])
+print(grid_b[0])
 grids = [grid, grid_a, grid_b]
