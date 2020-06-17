bb8a1849e064b88a4736d85ffe3afdc51926dd51
ardanwan <jozsef68@gmail.com>
2020-06-17 10:58:16 +0200

    Mating klaar (met offspring matrix)

--- spatial/spatial_CA.py
+++ spatial/spatial_CA.py
@@ -93,16 +93,87 @@ def mating(grids):
     S1 = S1ab + S1Ab + S1aB + S1AB
     S2 = S2ab + S2Ab + S2aB + S2AB
 
-    # Rearrange S variables to permutefor  the probabilities formula
+    # Rearrange S variables to permute for the probabilities formula
     Sab = (S1, S2, S1ab, S1Ab, S1aB, S1AB, S2ab, S2Ab, S2aB, S2AB)
     SAb = (S1, S2, S1Ab, S1ab, S1AB, S1aB, S2Ab, S2ab, S2AB, S2aB)
     SaB = (S1, S2, S1aB, S1AB, S1ab, S1Ab, S2aB, S2AB, S2ab, S2Ab)
     SAB = (S1, S2, S1AB, S1aB, S1Ab, S1ab, S2AB, S2aB, S2Ab, S2ab)
 
-    p_matrix = np.array([[probabilities(Sab)], [probabilities(SaB)],
-                        [probabilities(SAb)], [probabilities(SAB)]])
+    p_matrix = [probabilities(Sab), probabilities(SaB),
+                        probabilities(SAb), probabilities(SAB)]
 
-    return p_matrix
+    # Loop over grid and create offspring matrix
+    offspring_a = np.zeros((SIZE, SIZE))
+    offspring_b = np.zeros((SIZE, SIZE))
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+
+            p = np.random.uniform(0, 1)
+            if grid_a[j][i] == 0 & grid_b[j][i] == 0:
+                offspring_a[j][i] = 0
+                offspring_b[j][i] = 0
+
+            elif grid_a[j][i] == 1 & grid_b[j][i] == 1:
+                p1, p2, p3, p4 = p_matrix[0]
+                if p < p1:
+                    offspring_a[j][i] = 1
+                    offspring_b[j][i] = 1
+                elif p < p1 + p2:
+                    offspring_a[j][i] = 2
+                    offspring_b[j][i] = 2
+                elif p < p1 + p2 + p3:
+                    offspring_a[j][i] = 1
+                    offspring_b[j][i] = 2
+                else:
+                    offspring_a[j][i] = 2
+                    offspring_b[j][i] = 2
+
+            elif grid_a[j][i] == 1 & grid_b[j][i] == 2:
+                p1, p2, p3, p4 = p_matrix[1]
+                if p < p1:
+                    offspring_a[j][i] = 1
+                    offspring_b[j][i] = 2
+                elif p < p1 + p2:
+                    offspring_a[j][i] = 2
+                    offspring_b[j][i] = 2
+                elif p < p1 + p2 + p3:
+                    offspring_a[j][i] = 1
+                    offspring_b[j][i] = 1
+                else:
+                    offspring_a[j][i] = 2
+                    offspring_b[j][i] = 1
+
+            elif grid_a[j][i] == 2 & grid_b[j][i] == 1:
+                p1, p2, p3, p4 = p_matrix[2]
+                if p < p1:
+                    offspring_a[j][i] = 2
+                    offspring_b[j][i] = 2
+                elif p < p1 + p2:
+                    offspring_a[j][i] = 1
+                    offspring_b[j][i] = 1
+                elif p < p1 + p2 + p3:
+                    offspring_a[j][i] = 2
+                    offspring_b[j][i] = 2
+                else:
+                    offspring_a[j][i] = 1
+                    offspring_b[j][i] = 2
+
+            elif grid_a[j][i] == 2 & grid_b[j][i] == 2:
+                p1, p2, p3, p4 = p_matrix[3]
+                if p < p1:
+                    offspring_a[j][i] = 2
+                    offspring_b[j][i] = 2
+                elif p < p1 + p2:
+                    offspring_a[j][i] = 1
+                    offspring_b[j][i] = 2
+                elif p < p1 + p2 + p3:
+                    offspring_a[j][i] = 2
+                    offspring_b[j][i] = 1
+                else:
+                    offspring_a[j][i] = 1
+                    offspring_b[j][i] = 1
+
+    return offspring_a, offspring_b
 
 # Calculates probabilities
 def probabilities(S):
@@ -124,5 +195,3 @@ grid, grid_a, grid_b = initialise()
 
 grids = [grid, grid_a, grid_b]
 grids = survival(grids)
-
-print(mating(grids))
