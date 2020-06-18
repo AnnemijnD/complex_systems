532533f0829aeff9f8583667c5e749d0fb833652
ardanwan <jozsef68@gmail.com>
2020-06-18 13:35:34 +0200

    cleanup

--- spatial/experiments.py
+++ spatial/experiments.py
@@ -16,9 +16,7 @@ Optional arguments:
 """
 
 
-<<<<<<< HEAD
+
 figure = model.run_model(1000, size=50, empty=0.2, survive={1:0.8, 2:0.8}, p=0.85, grid_type="STRUCTURED")
-=======
 
 model.run_model(2000, size=20,survive={1:0.8, 2:0.8}, p=0.85, empty=0.2, grid_type="STRUCTURED")
->>>>>>> 71cf2c56b64a75398efd1a14a43290c981d05f44
