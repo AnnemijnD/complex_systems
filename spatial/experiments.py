248ad93f7111a5faf9a51d6047ca2e9a22491de1
ardanwan <jozsef68@gmail.com>
2020-06-18 11:27:26 +0200

    fixed probabilities

--- spatial/experiments.py
+++ spatial/experiments.py
@@ -16,4 +16,4 @@ Optional arguments:
 """
 
 
-figure = model.run_model(5000, size=50, survive={1:0.8, 2:0.8}, p=1, grid_type="STRUCTURED")
+figure = model.run_model(1000, size=50, empty=0, survive={1:0.8, 2:0.8}, p=1, grid_type="STRUCTURED")
