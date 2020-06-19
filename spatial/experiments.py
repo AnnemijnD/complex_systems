edba28d452f00bae95dde71200b95d2238edf2ac
ardanwan <jozsef68@gmail.com>
2020-06-19 13:34:27 +0200

    ...

--- spatial/experiments.py
+++ spatial/experiments.py
@@ -17,6 +17,4 @@ Optional arguments:
 
 
 
-figure = model.run_model(1000, size=50, empty=0.2, survive={1:0.8, 2:0.8}, p=0.85, grid_type="STRUCTURED")
-
-model.run_model(2000, size=20,survive={1:0.8, 2:0.8}, p=0.85, empty=0.2, grid_type="STRUCTURED")
+figure = model.run_model(1000, size=50, empty=0.1, survive={1:0.55, 2:0.55}, p=1, grid_type="STRUCTURED")
