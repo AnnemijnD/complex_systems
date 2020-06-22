4115c259e547349c942309e34a44b94ef2bf1625
AnnemijnD <annemijn1@live.nl>
2020-06-22 16:25:16 +0200

    met allopatric

--- spatial/experiments.py
+++ spatial/experiments.py
@@ -1,4 +1,5 @@
 import spatial_CA as model
+import spatial_CA_allopatric as allo_model
 import numpy as np
 
 """
@@ -17,6 +18,7 @@ Optional arguments:
 
 
 
-# model.run_model(20000, size=50,survive={1:0.8, 2:0.8}, p=1, empty=0.2, grid_type="STRUCTURED")
-model.run_model(20000, size=50, grid_type="NON_STRUCTURED", p=0.8, survive={1:0.85, 2:0.85}, empty=0)
-model.run_model(6000, size=20, grid_type="RANDOM", p=0.85, survive={1:0.8, 2:0.8}, empty=0)
+# model.run_model(1000, size=50,survive={1:0.8, 2:0.8}, p=1, empty=0.2, grid_type="STRUCTURED")
+# model.run_model(20000, size=50, grid_type="NON_STRUCTURED", p=0.8, survive={1:0.85, 2:0.85}, empty=0)
+# model.run_model(6000, size=20, grid_type="RANDOM", p=0.85, survive={1:0.8, 2:0.8}, empty=0)
+allo_model.run_model(1000, size=20, grid_type="STRUCTURED", empty=0, survive={1:0.95, 2:0.95}, p=0.5)
