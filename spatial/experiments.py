b86a1cab6249832c7d75b663d4411634188ebe2c
ardanwan <jozsef68@gmail.com>
2020-06-26 16:41:09 +0200

    ...

--- spatial/experiments.py
+++ spatial/experiments.py
@@ -2,6 +2,7 @@ import spatial_CA as model
 import spatial_CA_allopatric as allo_model
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 """
 Run the model through model.run_model(iterations, size, survive, p, empty, grid_type)
@@ -17,15 +18,55 @@ Optional arguments:
 - grid_type [str] (default is RANDOM), options: RANDOM, STRUCTURED, NON_STRUCTURED
 """
 
-#
-# for empty in [0, 0.1, 0.2]:
-#     for p in [0.8, 0.9, 1.0]:
-#         for x in np.arange(0.6, 1.0, 0.01):
-#             for z in range(10):
-#                 model.run_model(20000, size=50, empty=empty, survive={1:x, 2:x}, p=p, grid_type="NON_STRUCTURED", z=z)
+print(str(sys.argv))
+print(sys.argv[1], sys.argv[2])
+
+if len(sys.argv) != 3:
+    print("Please enter 2 arguments: MODEL (SYMPATRIC, ALLOPATRIC), TYPE(STANDARD, CUSTOM)")
+    exit()
+else:
+    if sys.argv[1] != ("SYMPATRIC" or "ALLOPATRIC") or sys.argv[2] != ("STANDARD" or "CUSTOM"):
+        print("Please enter 2 arguments: MODEL (SYMPATRIC, ALLOPATRIC), TYPE(STANDARD, CUSTOM)")
+    else:
+        if sys.argv[1] == "SYMPATRIC" and sys.argv[2] == "STANDARD":
+            print("Running model 1/5: 3 figures: 5000 iterations")
+            model.run_model(10000, size=50, empty=0, survive={1:0.8, 2:0.8}, p=0.85, grid_type="STRUCTURED")
+            print("Running model 2/5: 3 figures: 5000 iterations")
+            model.run_model(10000, size=50, empty=0, survive={1:0.85, 2:0.85}, p=1, grid_type="RANDOM")
+            print("Running model 3/5: 3 figures: 5000 iterations")
+            model.run_model(10000, size=50, empty=0, survive={1:0.7, 2:0.7}, p=1, grid_type="STRUCTURED")
+            print("Running model 4/5: 3 figures: 5000 iterations")
+            model.run_model(10000, size=50, empty=0, survive={1:0.64, 2:0.64}, p=1, grid_type="RANDOM")
+            print("Running model 5/5: 1 figure: 6 x max 5000 iterations (but probably less)")
+            times = []
+            ps = []
+            for x in np.arange(0.6, 1.0, 0.1):
+                time = []
+                for z in range(2):
+                    print(f"s = {x}, run {z+1}")
+                    time.append(model.run_model(5000, size=40, empty=0, survive={1:x, 2:x}, p=1, grid_type="STRUCTURED", plot=False, break_at_speciation=True))
+                times.append(np.average(time))
+                ps.append(x)
+                plt.scatter(ps, times)
+                plt.xlabel("survival s")
+                plt.ylabel("iteration")
+                plt.title("Speciation time (|LD - eps| > 0.245)")
+                plt.show()
+
+        elif sys.argv[1] == "SYMPATRIC" and sys.argv[2] == "CUSTOM":
+            pass
+
+        elif sys.argv[1] == "SYMPATRIC" and sys.argv[2] == "CUSTOM":
+            # Stnadaard allopatrisch resultaten
+            pass
+
+        elif sys.argv[1] == "SYMPATRIC" and sys.argv[2] == "CUSTOM":
+            # Custom sympatrisch resultaten
+            pass
+
 
 
 # model.run_model(1000, size=50,survive={1:0.8, 2:0.8}, p=1, empty=0.2, grid_type="STRUCTURED")
 # model.run_model(20000, size=50, grid_type="NON_STRUCTURED", p=0.8, survive={1:0.85, 2:0.85}, empty=0)
 # model.run_model(6000, size=20, grid_type="RANDOM", p=0.85, survive={1:0.8, 2:0.8}, empty=0)
-allo_model.run_model(1000, size=20, grid_type="STRUCTURED", empty=0, survive={1:0.95, 2:0.95}, p=0.5)
+# allo_model.run_model(1000, size=20, grid_type="STRUCTURED", empty=0, survive={1:0.95, 2:0.95}, p=0.5)
